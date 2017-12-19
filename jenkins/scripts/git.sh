c34c028d2abed2ec47c53bf6e0a8a668fa0b014f
Eddie Cheung <androidjaijai@gmail.com>
2017-12-20 02:59:06 +0800

    2222222222

--- jenkins/scripts/git.sh
+++ jenkins/scripts/git.sh
@@ -1,4 +1,5 @@
-git diff --stat $GIT_COMMIT $GIT_PREVIOUS_COMMIT;
+git diff --stat --exit-code $GIT_COMMIT $GIT_PREVIOUS_COMMIT -- app/config.js;
+echo $?;
 if [ $? == 0 ]; then
   echo "success"
 else
